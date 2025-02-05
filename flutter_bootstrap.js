10046cf32c4c96dfb22958a28d5bad876cc0bc09
Piyabordee Phongam <piyabordee.phongam@gmail.com>
2025-02-06 06:27:44 +0700

    Built web
    Branch: master
    Commit: 3b87d7f43d4d3ef2a6e660921944ceb2a86d4e1c (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2643176866"
+    serviceWorkerVersion: "1577061499"
   }
 });
